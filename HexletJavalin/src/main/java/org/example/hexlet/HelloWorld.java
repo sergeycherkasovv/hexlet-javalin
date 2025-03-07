97461014c4db1b4614f3978b4c46bfdf3edd5ed0
sergeycherkasovv <iamcherkasov.job@gmail.com>
2025-03-07 13:48:47 +0700

    changed

---
+++ HexletJavalin/src/main/java/org/example/hexlet/HelloWorld.java
@@ -0,0 +1,21 @@
+package org.example.hexlet;
+
+import io.javalin.Javalin;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Создаем приложение
+        var app = Javalin.create(config -> {
+            config.bundledPlugins.enableDevLogging();
+        });
+        // Описываем, что загрузится по адресу /
+        //app.get("/", ctx -> ctx.result("Hello World"));
+        app.get("/users", ctx -> ctx.result("GET /users"));
+        app.post("/users", ctx -> ctx.result("POST /users"));
+        app.get("/hello", ctx -> {
+            String name = ctx.queryParamAsClass("name", String.class).getOrDefault("World");
+            ctx.result("Hello " + name + "!");
+        });
+        app.start(7070); // Стартуем веб-сервер
+    }
+}
